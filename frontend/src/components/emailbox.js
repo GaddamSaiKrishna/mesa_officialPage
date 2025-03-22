36f232500235c03db9f53ec09c172467a170de73
unknown <gaddamsaikrishna460@gmail.com>
2025-03-22 21:25:03 +0530

    upd:emailbox

--- frontend/src/components/emailbox.js
+++ frontend/src/components/emailbox.js
@@ -35,12 +35,12 @@ export const ContactUs = () => {
     <div>
       <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="from_name" />
+        <input type="text" name="from_name"/>
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="user_email" required/>
         <label>Message</label>
         <textarea name="message" />
-        <input type="submit" value="Send" className="submit" id="liveToastBtn" />
+        <input type="submit" value="Send" className="submit" id="liveToastBtn" required/>
       </form>
 
       <div className="toast-container"
